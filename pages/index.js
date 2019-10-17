f01c0504920f550866bd4a022dc8e15cb9d2a992
Woody Evins <wevins@Woody-Evins.local>
2019-10-17 09:18:18 -0500

    add semantic ui and placeholder video

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Head from 'next/head';
+import { Container, Divider, Embed, Header } from 'semantic-ui-react'
+
 const Index = () => (
-    <div>
-      <p>Hello Next.js</p>
-    </div>
+    <Container text>
+      <Head>
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+      </Head>
+      <Divider hidden />
+      <Header>Spook Us Silly</Header>
+      <Embed>
+        <iframe width="100%" height="" src="https://www.youtube.com/embed/PBHvJrVma8Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </Embed>
+    </Container>
   );
   
-  export default Index;
+export default Index;
