5312b174937f5e4768bef50405802effd06768fb
Woody Evins <wevins@Woody-Evins.local>
2019-10-21 14:30:17 -0500

    add styled-components

---
+++ global/theme.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const Theme = {
+  colors: {
+    black: '#000000',
+    white: '#FFFFFF'
+  }
+};
+
+export const Resets = createGlobalStyle`
+  #__next, html, body {
+    background-color: ${p => p.theme.colors.black}
+    color: ${p => p.theme.colors.white}
+    height: 100vh;
+    margin: 0;
+    width: 100vw;
+  }
+`;
