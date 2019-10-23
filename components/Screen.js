979627466102bbebacc08504d3277c39a8685535
Woody Evins <wevins@Woody-Evins.local>
2019-10-23 16:11:30 -0500

    allow users to turn off tv

--- components/Screen.js
+++ components/Screen.js
@@ -7,25 +7,18 @@ const Player = styled.iframe`
   border-width: 1px;
   box-shadow: inset 0 0 5px 5px #252e25;
   height: ${p => p.screenwidth * 0.5 * 0.75}px;
-  opacity: 1;
+  opacity: ${p => (p.tvOn ? 1 : 0)};
   overflow: hidden;
   width: ${p => p.screenwidth * 0.5}px;
   z-index: 10;
 `;
 
-const Wrapper = styled.div`
-  background: radial-gradient(#888888, #252e25);
-  padding: 10px;
-  position: relative;
-  width: ${p =>
-    p.screenwidth > `1000` ? p.screenwidth * 0.55 : p.screenwidth * 0.9}px;
-`;
-
-const Screen = ({ screenwidth }) => (
+const Screen = ({ screenwidth, tvOn }) => (
   <Player
     frameBorder="0"
     screenwidth={screenwidth}
     src="https://player.twitch.tv/?channel=dcastkid&autoplay=1&muted=1"
+    tvOn={tvOn}
   />
 );
 
